a2544fa252c5b182b62c4a104d70e2f8c11e07ee
mgstet <68048265+mgstet@users.noreply.github.com>
2023-08-13 03:17:29 +0200

    Update subsyncer.py

--- subsyncer.py
+++ subsyncer.py
@@ -11,10 +11,11 @@ subs = glob.glob("*.srt")
 os.mkdir("temp")
 
 for vid, sub in zip(vids, subs):
-    command = f"alass -O 0 -g -p 5 {vid} {sub} temp/{sub}".split(" ")
+    command = ["alass", "-O", "0", "-g", "-p", "5", f"{vid}", f"{sub}", f"temp/{sub}"]
     subprocess.run(command)
 
 for sub in subs:
     shutil.move(f"temp/{sub}", sub)
 
 os.rmdir("temp")
+
