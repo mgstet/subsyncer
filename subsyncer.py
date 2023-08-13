46913bc0940b0098415e474575f945ba21feddf4
mgstet <68048265+mgstet@users.noreply.github.com>
2023-08-13 02:05:23 +0200

    Add files via upload

---
+++ subsyncer.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import shutil
+import glob
+
+vids = glob.glob("*.mkv")
+subs = glob.glob("*.srt")
+
+os.mkdir("temp")
+
+for vid, sub in zip(vids, subs):
+    command = f"alass -O 0 -g -p 5 {vid} {sub} temp/{sub}".split(" ")
+    subprocess.run(command)
+
+for sub in subs:
+    shutil.move(f"temp/{sub}", sub)
+
+os.rmdir("temp")
